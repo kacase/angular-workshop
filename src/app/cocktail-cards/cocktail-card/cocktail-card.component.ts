af848d9a94bc6ba436196545923e74980c3d61b3
Eriksen, Jan Olaf <jaeriksen@deloitte.de>
2021-01-13 12:23:01 +0100

    ein paar changes

--- src/app/cocktail-cards/cocktail-card/cocktail-card.component.ts
+++ src/app/cocktail-cards/cocktail-card/cocktail-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import Cocktail from '../../../models/Cocktail';
 
