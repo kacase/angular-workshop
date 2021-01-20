de65753afadb3d1aaf6d2468cec20bff526f2ce7
Ehser, Christian Laurin <cehser@deloitte.de>
2021-01-20 16:47:56 +0100

    added unit test for cocktaildetails()

--- src/app/services/cocktail.service.spec.ts
+++ src/app/services/cocktail.service.spec.ts
@@ -78,27 +78,27 @@ describe('HttpClient testing', () => {
     httpTestingController.verify();
   });
 
-  // beforeEach(() => {
-  //   // TODO: spy on other methods too
-  //   httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-  //   cocktailService = new CocktailService(httpClientSpy as any);
-  // });
+  beforeEach(() => {
+    // TODO: spy on other methods too
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    cocktailService = new CocktailService(httpClientSpy as any);
+  });
 
-  // it('should return expected cocktail (HttpClient called once)', () => {
-  //   const expectedCocktail: Cocktail = new Cocktail();
-  //   console.log('test cocktail', expectedCocktail);
+  it('should return expected cocktail (HttpClient called once)', () => {
+    const expectedCocktail: Cocktail = new Cocktail();
+    console.log('test cocktail', expectedCocktail);
 
-  //   httpClientSpy.get.and.returnValue(of(expectedCocktail));
+    httpClientSpy.get.and.returnValue(of(expectedCocktail));
 
-  //   cocktailService.getCocktailDetails('12089').subscribe(
+    cocktailService.getCocktailDetails('12089').subscribe(
 
-  //     cocktail => {
-  //       console.log('cocktail from api', cocktail);
-  //       expect(cocktail).toEqual(expectedCocktail, 'expected cocktail')
-  //     },
-  //     fail
-  //   );
-  //   expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
-  // });
+      cocktail => {
+        console.log('cocktail from api', cocktail);
+        expect(cocktail).toEqual(expectedCocktail, 'expected cocktail')
+      },
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
 
 });
